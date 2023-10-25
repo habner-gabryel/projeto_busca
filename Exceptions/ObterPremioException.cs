39405549831b162106fae080170fe66f832453e1
Habner Gabryel <habner.gabryel.cc@gmail.com>
2023-10-25 16:33:22 -0300

    Commit - 5 - Criação de exception personalizada e tratamento de possíveis erros na classe Mapa

---
+++ Exceptions/ObterPremioException.cs
@@ -0,0 +1,15 @@
+﻿namespace projeto_busca.Exceptions
+{
+    [System.Serializable]
+    public class OberPremioExeption : System.Exception
+    {
+        public OberPremioExeption() { }
+        public OberPremioExeption(string message) : base(message) { }
+        public OberPremioExeption(string message, System.Exception inner) : base(message, inner) { }
+        protected OberPremioExeption(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    }
+ 
+}
--- classes/Mapa.cs
+++ classes/Mapa.cs
@@ -1,27 +1,44 @@
-﻿using System.Configuration;
+﻿using projeto_busca.Exceptions;
+using System.Configuration;
 
-namespace projeto_busca.classes
+namespace projeto_busca.Classes
 {
     class Mapa
     {
         public int linhas {  get; set; }
         public int colunas { get; set; }
-        private Premio[,] premios;
+        private Premio[] premios;
         private Saida saida;
+        private TerrenoPosicao[] tps;
 
-        public Mapa(int linhas, int colunas, Saida saida) { 
+        public Mapa(int linhas, int colunas, Premio[] premios, Saida saida, TerrenoPosicao[] tps) { 
             this.linhas = linhas; 
             this.colunas = colunas;
-            this.premios = new Premio[linhas, colunas];
+            this.premios = premios;
             this.saida = saida;
+            this.tps = tps;
         }
 
-        public Premio ObterPremio(int linha, int coluna) { 
-            return premios[linha, coluna];
+        public Premio ObterPremio(Posicao posicao) {
+            Premio premio = null;
+            foreach(Premio p in this.premios) {
+                if(p.terrenoPosicao != null && p.terrenoPosicao.posicao != null && p.terrenoPosicao.posicao == posicao)
+                {
+                    premio = p;
+                }
+            }
+
+            if(premio == null) {
+                throw new OberPremioExeption("Nenhum premio encontrado para a posicao: " + posicao.ToString() );
+            }
+            return premio;
         }
 
-        public Saida obterSaida(int linha, int coluna) {
-            return saida;
+        public Saida obterSaida() {
+            if(this.saida == null || this.saida.posicao == null) {
+                throw new System.Exception("Saida inexistente no mapa");
+            }
+           return this.saida;
         }
     }
 }
