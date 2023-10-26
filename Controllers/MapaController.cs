81721851406ea6f8273df9a5d783a3dc9d94b301
Habner Gabryel <habner.gabryel.cc@gmail.com>
2023-10-26 19:51:46 -0300

    Commit - 10 - Remoção de função de imprimir mapa do controller

--- Controllers/MapaController.cs
+++ Controllers/MapaController.cs
@@ -2,37 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Documents;
 
 namespace projeto_busca.Controllers
 {
     class MapaController
     {
-        public static void imprimirMapa(Mapa mapa) {
-
-            if (mapa == null)
-            {
-                throw new System.ArgumentNullException("mapa");
-            }
-
-            for (int x = 0; x <= mapa.linhas; x++) {
-                for (int y = 0; y <= mapa.colunas; y++) {
-
-                    if (mapa.obterSaida().posicao == new Posicao(x, y))
-                    {
-                        // --- print da saida ---
-                    }
-
-                    if (mapa.ObterPremio(new Posicao(x, y)) != null)
-                    {
-                        // --- print do premio ---
-                    }
-
-                }
-            }
-        }
-
-        public Mapa criarMapa(int linhas, int colunas, int heuristica) { 
+        public static Mapa criarMapa(int linhas, int colunas)
+        {
             Random random = new Random();
             int quantPremios = random.Next(1, 7);
 
@@ -41,23 +17,27 @@ namespace projeto_busca.Controllers
             Array terrenosDisp = Enum.GetValues(typeof(Terreno));
 
 
-            for (int x = 0; x <= linhas; x++) {
-                for (int y = 0; y <= colunas; y++) {
+            for (int x = 0; x <= linhas; x++)
+            {
+                for (int y = 0; y <= colunas; y++)
+                {
                     posicoes.Add(new TerrenoPosicao((Terreno)terrenosDisp.GetValue(random.Next(terrenosDisp.Length)), new Posicao(x, y)));
                 }
             }
 
-            for (int x = 0; x <= quantPremios; x++) {
-                premios.Add( new Premio((TerrenoPosicao)posicoes.ElementAt(random.Next(posicoes.Count)), random.Next(5,10)));
+            for (int x = 0; x <= quantPremios; x++)
+            {
+                premios.Add(new Premio((TerrenoPosicao)posicoes.ElementAt(random.Next(posicoes.Count)), random.Next(5, 10)));
             }
 
-            List<TerrenoPosicao> posPremios = new List<TerrenoPosicao>(); 
+            List<TerrenoPosicao> posPremios = new List<TerrenoPosicao>();
 
-            foreach(Premio premio in premios) {
+            foreach (Premio premio in premios)
+            {
                 posPremios.Add(premio.terrenoPosicao);
             }
 
-            Saida saida = new Saida(((TerrenoPosicao)posicoes.ElementAt(random.Next(posicoes.Count))).posicao, heuristica);
+            Saida saida = new Saida(((TerrenoPosicao)posicoes.ElementAt(random.Next(posicoes.Count))).posicao);
 
             return new Mapa(linhas, colunas, premios, saida, posicoes);
         }
